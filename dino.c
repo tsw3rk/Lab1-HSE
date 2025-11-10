5712702154b08b511359857d1f5c8ae6ae772b67
t1m <nurbaevt929@gmail.com>
2025-11-11 01:36:49 +0300

    Реализована функция
    Добавлена проверка на успешное выделение памяти

--- dino.c
+++ dino.c
@@ -4,12 +4,15 @@
 
 Dino* createDino(int x, int y) {
     Dino* d = malloc(sizeof(Dino));
-    d->x = x;
-    d->y = y;
+    if (d != NULL) {  // Проверка успешно ли выделась память
+        d->x = x;
+        d->y = y;
+    }
     return d;
 }
 
 int moveDino(Dino* d, int dx, int dy, int width, int height) {
+    //Логика перемещения по тору
     d->x = (d->x + dx + width) % width;
     d->y = (d->y + dy + height) % height;
     return 1;
