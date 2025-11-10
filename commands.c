53f3a215bb43e5fb91f6f2bdba6b7c5116345710
t1m <nurbaevt929@gmail.com>
2025-11-11 00:08:02 +0300

    Добавлены новые команды DIG и MOUND
    Реализованы правила из части 2 и функции DIG и MOUND

--- commands.c
+++ commands.c
@@ -3,7 +3,8 @@
 #include <string.h>
 
 // 1 - exelent(успех, правильный ввод, все хорошо)
-// 0 - error(будет сообщена ошибка, неверный ввод, все плохо)
+// 0 - error(будет сообщена ошибка, неверный ввод)
+// -1 -Error(критическое состание, завершение)
 
 int executeCommand(Field* f, Dino* d, char* line) {
     char cmd[20];
@@ -28,8 +29,8 @@ int executeCommand(Field* f, Dino* d, char* line) {
                 return 1;           //exelent
             }
         }
-        return 0;
 
+        return 0;
     }
 
     else if (strcmp(cmd, "MOVE") == 0) {
@@ -40,18 +41,104 @@ int executeCommand(Field* f, Dino* d, char* line) {
             else if (strcmp(direction, "DOWN") == 0){dy = 1;}
             else if (strcmp(direction, "LEFT") == 0){dx = -1;}
             else if (strcmp(direction, "RIGHT") == 0){dx = 1;}
-            else return 0;// неизвестное направление
+            else {return 0;}                // неизвестное направление
+
+            int newX = d->x + dx;
+            int newY = d->y + dy;
+
+            newX = (newX + f->width) % f->width;
+            newY = (newY + f->height) % f->height;
+
+            char targetCell = f->grid[newY][newX];
+
+            if (targetCell == '%') {                                         //Яма правило 1
+                printf("Ошибка: Дино наступил на яму (%d, %d). Завершение.\n", newX, newY);
+                return -1;
+            }else if (targetCell == '^') {
+                printf("Предупреждение: Невозможно движение на гору (%d, %d). Комманда игнорируется.\n", newX, newY);
+                return 1;
+            }
+
+            //Продолжаем движение, стираем клетку, если покрашенна, то возвращаем цвет, иначе пустая клетка.
+
             if (f->paintGrid[d->y][d->x] != 0) {
                 f->grid[d->y][d->x] = f->paintGrid[d->y][d->x];
             } else {
                 f->grid[d->y][d->x] = '_';
             }
-            moveDino(d, dx, dy, f->width, f->height);
+
+            d->x = newX;
+            d->y = newY;
+
             f->grid[d->y][d->x] = '#';
+
+            return 1;
+        }
+        return 0;
+
+    }
+
+    else if (strcmp(cmd, "PAINT")==0) {
+        char color;  //временный цвет (a-z)
+        if (sscanf(line, "PAINT %c", &color) == 1) {
+            if (color >= 'a' && color <= 'z') {
+                f->paintGrid[d->y][d->x] = color;
+                if (f->grid[d->y][d->x] == '#') {
+                    //esli dino стоял он закрывает цвет не нужно менять символ, т к должно быть видно дино
+                } else {
+                    f->grid[d->y][d->x] = color;
+                }
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    else if (strcmp(cmd, "DIG")==0) {
+        char direction[10];
+        if (sscanf(line, "DIG %s", direction) == 1) {
+            int startPosition_x = d->x;
+            int startPosition_y = d->y; // Начальное положение нашего дInO
+
+            //Определяем соседнюю клетку
+
+            if (strcmp(direction, "UP") == 0){startPosition_y--;}
+            else if (strcmp(direction, "DOWN") == 0){startPosition_y++;}
+            else if (strcmp(direction, "LEFT") == 0){startPosition_x--;}
+            else if (strcmp(direction, "RIGHT") == 0){startPosition_x++;}
+            else {return 0;}
+
+            //Логика перемещения по тору
+            startPosition_x = (startPosition_x  + f->width) % f->width;
+            startPosition_y = (startPosition_y + f->height) % f->height;
+
+            f->grid[startPosition_y][startPosition_x] = '%';
+            return 1;
+        }
+
+        return 0;
+
+    } else if (strcmp(cmd, "MOUND")==0) {
+        char direction[10];
+        if (sscanf(line, "MOUNT %s", direction) == 1) {
+            int startPosition_x = d->x;
+            int startPosition_y = d->y;
+            if (strcmp(direction, "UP") == 0){startPosition_y--;}
+            else if (strcmp(direction, "DOWN") == 0){startPosition_y++;}
+            else if (strcmp(direction, "LEFT") == 0){startPosition_x--;}
+            else if (strcmp(direction, "RIGHT") == 0){startPosition_x++;}
+            else {return 0;}
+
+            startPosition_x = (startPosition_x + f->width) % f->width;
+            startPosition_y = (startPosition_y + f->height) % f->height;
+
+            if (f->grid[startPosition_y][startPosition_x] == '%') {  //Насыпание горы в яму ---> клетка становиться пустой
+                f->grid[startPosition_y][startPosition_x] = '_';
+            } else {
+                f->grid[startPosition_y][startPosition_x] = '^';
+            }
             return 1;
         }
         return 0;
     }
-    // Будут и другие комманды
-    return 0;
 }
