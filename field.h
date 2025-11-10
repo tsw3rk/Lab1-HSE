84880ad1fa27c283316f0dc9132dcaa9f7dfeddc
t1m <nurbaevt929@gmail.com>
2025-11-10 20:42:57 +0300

    Добавлены объявление структуры Field и функции create_field
    - Определена структура Field для хранения размеров и данных поля.
    - Объявлена функция create_field для выделения памяти и инициализации экземпляра Field.
    - Также объявлены функции print_field и free_field для дальнейшей реализации.

--- field.h
+++ field.h
@@ -1,5 +1,19 @@
-
 #ifndef LAB1_FIELD_H
 #define LAB1_FIELD_H
+
+#define MAX_WIDTH 100
+#define MAX_HEIGHT 100
+#define MAX_CELL_VALUE 'z'  // for painting
+
+typedef struct {
+    int width;
+    int height;
+    char grid[MAX_HEIGHT][MAX_WIDTH];  // символы на поле
+    char paint_grid[MAX_HEIGHT][MAX_WIDTH];  // цвета клеток (a-z)
+} Field;
+
+Field* create_field(int width, int height);
+void print_field(Field* f);
+void free_field(Field* f);
 
 #endif //LAB1_FIELD_H
