c2ebc5e4c7f909cc8575da4d9a379117f77872f8
t1m <nurbaevt929@gmail.com>
2025-11-11 01:38:35 +0300

    Забыл добавить сюда обозначения ошибок
    Добавил!!

--- commands.h
+++ commands.h
@@ -1,5 +1,8 @@
 #ifndef LAB1_COMMANDS_H
 #define LAB1_COMMANDS_H
+#define EXEC_SUCCESS 1      // Команда выполнена успешно
+#define EXEC_ERROR_FORMAT 0 // Некорректный формат команды или аргументы
+#define EXEC_ERROR_FATAL -1 // Критическая ошибка (например, наступление в яму)
 
 #include "field.h"
 #include "dino.h"
