1f386dbfd0d45279eaa323eed702ad53c5d1b354
t1m <nurbaevt929@gmail.com>
2025-11-11 02:03:40 +0300

    Создана структура в частности для команды EXEC
    Просто из main файла перенес сюда и добавил новое поля для попытки избежать рекурсию

--- commands.h
+++ commands.h
@@ -4,6 +4,21 @@
 #define EXEC_ERROR_FORMAT 0 // Некорректный формат команды или аргументы
 #define EXEC_ERROR_FATAL -1 // Критическая ошибка (например, наступление в яму)
 
+typedef struct {
+    int sizeWasCalled;  // 1 если SIZE была, 0 если нет
+    int startWasCalled; // 1 если START была, 0 если нет
+    int lineNumber;     // текущий номер строки
+
+    // Новые поля для опций
+    int noDisplay;
+    int noSave;
+    int intervalSeconds;
+
+    // Новое поле для отслеживания вложенности EXEC
+    int execDepth;
+
+} ExecutionState;
+
 #include "field.h"
 #include "dino.h"
 
